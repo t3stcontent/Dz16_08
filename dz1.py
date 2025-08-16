3aa1c16274bc05aed61070dcb8da45fa8ad25408
teacher <>
2025-08-16 13:03:27 +0300

    fix set

--- dz1.py
+++ dz1.py
@@ -1,1 +1,15 @@
 print("My homework")
+
+text = [1,12,3,123,1,23,12,3,123,12,3,2,2,2,23,12,312,3,123,12,3,123,12,312,312312,3123123,5,5,5,5,5,5,5]
+
+
+elements_to_remove = [5, 2]
+
+res = []
+exp_set = set(elements_to_remove)
+for i in text:
+    if i in exp_set:
+        continue
+    res.append(i)
+
+print(res)
